025a06f00f73d6003fb79de5aa15e89284a0aa16
Michał <hortexix@gmail.com>
2020-07-16 09:17:16 +0200

    Updates

---
+++ precache-manifest.07b1e933364a5fab11657a3f0820ac1b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8f6d6b108f980af982afe57b823078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f006fb1d368a609a3e14",
+    "url": "/static/css/main.d31faeea.chunk.css"
+  },
+  {
+    "revision": "f8f6017bfccd6718fad4",
+    "url": "/static/js/2.b0aaa5fa.chunk.js"
+  },
+  {
+    "revision": "65f773f722c21ee7574673635baa1057",
+    "url": "/static/js/2.b0aaa5fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f006fb1d368a609a3e14",
+    "url": "/static/js/main.08180acf.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a8eb7421f0a4b62bccc8eda88347a7e2",
+    "url": "/static/media/bookS.a8eb7421.png"
+  },
+  {
+    "revision": "f8085ff42b5203f32892fd6ef1079cbf",
+    "url": "/static/media/logo.f8085ff4.png"
+  },
+  {
+    "revision": "5580c92009c1bf794f486e5f64ba1110",
+    "url": "/static/media/mate-checker.5580c920.png"
+  }
+]);
